1646a0695267608751f1e671ad73759591216405
Jhevison <jhevison74@gmail.com>
2020-01-13 22:44:23 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,31 @@
+
+case `dpkg --print-architecture` in
+
+aarch64) pegar="arm64"
+;;
+arm) pegar="armhf"
+;;
+i*86) pegar="i386"
+;;
+x86_64) pegar= "amd64"
+;;
+*) echo "arquitetura desconhecida"
+;;
+esac
+pkg install debootstrap proot wget
+clear
+echo "Baixando Debian 9"
+echo ""
+echo ""
+debootstrap --arch=$pegar stretch debian http://ftp.de.debian.org/debian
+
+touch start.sh
+chmod +x start.sh
+echo "unset LD_PRELOAD
+
+#!/data/data/com.termux/files/usr/bin/sh
+proot -0 -r ~/debian -b /dev/ -b /sys/ -b /proc/ -b /data/data/com.termux/files/home /usr/bin/env -i HOME=/root TERM="xterm-256color" PS1='[root@stable \W]$ ' PATH=/bin:/usr/bin:/sbin:/usr/sbin:/bin /bin/bash --login" >> start.sh
+
+clear
+echo "Rode o ./start.sh para iniciar o Debian (run the ./start.sh to run the Debian)"
+
